34f2270fdcc2713bc4c1bf7fba0a05abf452f781
zickbalazs <zickbalazs@turr.hu>
2022-11-07 14:09:39 +0100

    Működő app

--- js/app.js
+++ js/app.js
@@ -5,7 +5,27 @@ app.run(function($rootScope){
     $rootScope.author = 'Zick Balázs';
     $rootScope.company = 'Türr Tech';
     $rootScope.classes = [];
-    if (window.sessionStorage.getItem('angular-orarend')!=null) $rootScope.classes = angular.fromJson(window.localStorage.getItem('angular-orarend'));
+    $rootScope.errormessage = "";
+    $rootScope.icon = "";
+    $rootScope.ErrorMessage = false;
+    $rootScope.CanEdit = false;
+    $rootScope.errortype = "";
+    $rootScope.Remove = function(index){
+        $rootScope.errormessage = "Óra törölve";
+        $rootScope.errortype = "primary";
+        $rootScope.icon = "bi-info-circle";
+        $rootScope.ErrorMessage = true;
+        $rootScope.CanEdit = false;
+        $rootScope.classes.splice($rootScope.classes.findIndex(e=>e==index), 1);
+        $rootScope.SaveLocal();
+    };
+    $rootScope.Print = function(){
+        window.print();
+    }
+    $rootScope.SaveLocal = function(){
+        window.localStorage.setItem('angular-orarend', angular.toJson($rootScope.classes));
+    }
+    if (window.localStorage.getItem('angular-orarend')!=null) $rootScope.classes = angular.fromJson(window.localStorage.getItem('angular-orarend'));
 });
 
 
--- js/controller/formController.js
+++ js/controller/formController.js
@@ -1,7 +1,72 @@
 app.controller("form", function($scope, $rootScope){
     $scope.NewClass = {};
+    $scope.ChangeID = -1;
     $scope.AddClass = function(){
-        console.log($scope.NewClass)
+        console.log($scope.NewClass.teacher)
+        if (!$scope.CorrectData()){
+            $rootScope.errormessage = "Hibás adatok!"
+            $rootScope.errortype = "danger";
+            $rootScope.icon = "bi-exclamation-circle";
+            $rootScope.ErrorMessage = true;
+        }
+        else{
+            let scope = $scope.NewClass;
+            $rootScope.classes.push({
+                day:scope.day,
+                place:scope.place,
+                subject:scope.subject,
+                teacher:scope.teacher,
+                time:scope.time
+            });
+            $rootScope.SaveLocal();
+            $rootScope.errormessage = "Sikeres mentés"
+            $rootScope.errortype = "success";
+            $rootScope.icon = "bi-check-circle";
+            $rootScope.ErrorMessage = true;
+        }
     }
-
+    $scope.CorrectData = function(){
+        let scope = $scope.NewClass;
+        return scope.day != undefined && scope.place != undefined && scope.subject != undefined && scope.time != undefined && scope.teacher != undefined && scope.teacher != ''
+    }
+    $scope.Cancel = function(){
+        $rootScope.CanEdit = false;
+        $scope.NewClass = {};
+    }
+    $rootScope.Edit = function(index){
+        $scope.ChangeID = $rootScope.classes.findIndex(e=>e==index);
+        $scope.NewClass = {
+            day:index.day,
+            place:index.place,
+            subject:index.subject,
+            teacher:index.teacher,
+            time:index.time
+        };
+        $rootScope.CanEdit=true;
+    }
+    $rootScope.EditClass = function(){
+        if ($scope.CorrectData()){
+            let scope = $scope.NewClass;
+            $rootScope.classes[$scope.ChangeID] = {
+                day:scope.day,
+                place:scope.place,
+                subject:scope.subject,
+                teacher:scope.teacher,
+                time:scope.time
+            };
+            $rootScope.CanEdit = false;   
+            $scope.NewClass = {};     
+            $rootScope.SaveLocal();
+            $rootScope.icon = "bi-check-circle";
+            $rootScope.errormessage="Sikeres módosítás!";
+            $rootScope.errortype="success";
+            $rootScope.ErrorMessage=true;
+        }
+        else{
+            $rootScope.errormessage = "Hibás adatok!"
+            $rootScope.errortype = "danger";
+            $rootScope.icon = "bi-exclamation-circle";
+            $rootScope.ErrorMessage = true;
+        }
+    }
 })
